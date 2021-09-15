26c4cec3a43dc8300acde0bf907643bca2d1537f
BOLLIMUNTHA AVINASH <90610722+avinashmech@users.noreply.github.com>
2021-09-15 16:41:57 +0530

    Add files via upload

---
+++ vscode/D_ifelseExample2.java
@@ -0,0 +1,18 @@
+package decisionStatement;
+
+public class D_ifelseExample2 {
+public static void main(String[] args)
+{
+	int dinga=20;
+	int juice=50;
+	if(dinga>=juice)
+	{
+		System.out.println("Mango juice");
+	}
+    else 
+	{
+		System.out.println("apple juice");
+	}
+	
+}
+}
